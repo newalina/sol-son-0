335be3c6af9fb8cde6fe7455ee28639ee8abcf66
newalina <157907056+newalina@users.noreply.github.com>
2024-07-16 16:00:21 -0400

    add next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {
+    CLIENT_ID: process.env.CLIENT_ID,
+    CLIENT_SECRET: process.env.CLIENT_SECRET,
+    REDIRECT_URI: process.env.REDIRECT_URI,
+    SPREADSHEET_ID: process.env.SPREADSHEET_ID,
+    CREDENTIALS_PATH: process.env.CREDENTIALS_PATH,
+    TOKEN_PATH: process.env.TOKEN_PATH,
+  },
+};
 
 export default nextConfig;
