61588a66e73aef7900f5240c8c53b97428b526be
newalina <157907056+newalina@users.noreply.github.com>
2024-07-16 23:03:54 -0400

    install google-auth-library

--- app/api/googleSheets.js
+++ app/api/googleSheets.js
@@ -1,47 +1,42 @@
 require("dotenv").config();
-
+const { OAuth2Client } = require("google-auth-library");
 const { google } = require("googleapis");
 const fs = require("fs");
 const path = require("path");
+const keys = require("../../credentials/oauth2.keys.json");
 
-const clientId = process.env.CLIENT_ID;
-const clientSecret = process.env.CLIENT_SECRET;
-const redirectUri = process.env.REDIRECT_URI;
+const clientId = keys.web.client_id;
+const clientSecret = keys.web.client_secret;
+const redirectUri = keys.web.redirect_uris[0];
 const spreadsheetId = process.env.SPREADSHEET_ID;
 
-const token = {
-  access_token: process.env.ACCESS_TOKEN,
-  refresh_token: process.env.REFRESH_TOKEN,
-  scope: process.env.SCOPE,
-  token_type: process.env.TOKEN_TYPE,
-  expiry_date: parseInt(process.env.EXPIRY_DATE, 10),
-};
-
-const credentialsPath = path.resolve(
-  process.cwd(),
-  process.env.CREDENTIALS_PATH || "credentials/oauth2.keys.json"
-);
-const tokenPath = path.resolve(process.cwd(), process.env.TOKEN_PATH);
+let oAuth2Client;
 
 const authenticate = async () => {
-  const oAuth2Client = new google.auth.OAuth2(
-    clientId,
-    clientSecret,
-    redirectUri
-  );
+  oAuth2Client = new OAuth2Client(clientId, clientSecret, redirectUri);
 
-  oAuth2Client.setCredentials(token);
-
-  console.log("client id:", clientId ? clientId : "Not Loaded");
-  console.log("credentials path:", credentialsPath);
-  console.log("token:", token);
-
-  // if (fs.existsSync(tokenPath)) {
-  //   const token = fs.readFileSync(tokenPath, { encoding: "utf-8" });
-  //   oAuth2Client.setCredentials(JSON.parse(token));
-  // }
+  // Check for previously stored tokens
+  const tokenPath = path.resolve(process.cwd(), process.env.TOKEN_PATH);
+  if (fs.existsSync(tokenPath)) {
+    const token = fs.readFileSync(tokenPath, "utf-8");
+    oAuth2Client.setCredentials(JSON.parse(token));
+  } else {
+    // If no token exists, initiate the authorization flow
+    await getNewToken(oAuth2Client);
+  }
 
   return oAuth2Client;
+};
+
+const getNewToken = async (oAuth2Client) => {
+  const authorizeUrl = oAuth2Client.generateAuthUrl({
+    access_type: "offline",
+    scope: "https://www.googleapis.com/auth/spreadsheets",
+    prompt: "consent",
+  });
+
+  console.log("Authorize this app by visiting this url:", authorizeUrl);
+  // Further steps can be implemented to handle the callback and save tokens
 };
 
 const appendToSheet = async (data) => {
@@ -49,7 +44,7 @@ const appendToSheet = async (data) => {
   const sheets = google.sheets({ version: "v4", auth });
 
   await sheets.spreadsheets.values.append({
-    spreadsheetId: spreadsheetId,
+    spreadsheetId,
     range: "Sheet1!A:D",
     valueInputOption: "USER_ENTERED",
     requestBody: {
