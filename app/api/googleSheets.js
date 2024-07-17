2bb28b55c3cd3907b8c78a09f50439c78f270f42
newalina <157907056+newalina@users.noreply.github.com>
2024-07-17 00:31:36 -0400

    parse keys and token

--- app/api/googleSheets.js
+++ app/api/googleSheets.js
@@ -4,15 +4,15 @@ const { google } = require("googleapis");
 const fs = require("fs");
 const path = require("path");
 
-const credentialsPath = path.resolve(process.cwd(), "credentials");
+// const credentialsPath = path.resolve(process.cwd(), "credentials");
 
-fs.readdir(credentialsPath, (err, files) => {
-  if (err) {
-    console.error("Error reading credentials directory:", err);
-  } else {
-    console.log("Files in credentials directory:", files);
-  }
-});
+// fs.readdir(credentialsPath, (err, files) => {
+//   if (err) {
+//     console.error("Error reading credentials directory:", err);
+//   } else {
+//     console.log("Files in credentials directory:", files);
+//   }
+// });
 
 // const credentialsPath = path.resolve(
 //   process.cwd(),
@@ -21,27 +21,37 @@ fs.readdir(credentialsPath, (err, files) => {
 
 // const keys = JSON.parse(fs.readFileSync(credentialsPath, "utf8"));
 
-let keys;
-try {
-  keys = JSON.parse(fs.readFileSync(credentialsPath, "utf8"));
-  console.log("Loaded credentials successfully");
-} catch (error) {
-  console.error("Error reading credentials:", error);
-  throw new Error("Failed to load credentials");
-}
+// let keys;
+// try {
+//   keys = JSON.parse(fs.readFileSync(credentialsPath, "utf8"));
+//   console.log("Loaded credentials successfully");
+// } catch (error) {
+//   console.error("Error reading credentials:", error);
+//   throw new Error("Failed to load credentials");
+// }
+
+const keys = JSON.parse(process.env.GOOGLE_OAUTH2_KEYS);
+const token = JSON.parse(process.env.GOOGLE_TOKEN);
+
+console.log("KEYS: ", keys);
+console.log("TOKEN: ", token);
 
 const clientId = keys.web.client_id;
 const clientSecret = keys.web.client_secret;
 const redirectUri = keys.web.redirect_uris[0];
 const spreadsheetId = process.env.SPREADSHEET_ID;
 
-const token = {
-  access_token: process.env.ACCESS_TOKEN,
-  refresh_token: process.env.REFRESH_TOKEN,
-  scope: process.env.SCOPE,
-  token_type: process.env.TOKEN_TYPE,
-  expiry_date: parseInt(process.env.EXPIRY_DATE, 10),
-};
+console.log("clientId: ", clientId);
+console.log("clientSecret: ", clientSecret);
+console.log("redirectUri: ", keys.web.redirect_uris[0]);
+
+// const token = {
+//   access_token: process.env.ACCESS_TOKEN,
+//   refresh_token: process.env.REFRESH_TOKEN,
+//   scope: process.env.SCOPE,
+//   token_type: process.env.TOKEN_TYPE,
+//   expiry_date: parseInt(process.env.EXPIRY_DATE, 10),
+// };
 
 const authenticate = async () => {
   const oAuth2Client = new google.auth.OAuth2(
