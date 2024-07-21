6b8d2fbcd1a0dd8fefc07a8220fa76f624f1a765
newalina <157907056+newalina@users.noreply.github.com>
2024-07-21 16:12:49 -0400

    add viewport meta tag

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useState } from "react";
+import Head from "next/head";
 import Image from "next/image";
 import styles from "./page.module.css";
 import { Header } from "./components/Header";
@@ -8,14 +9,20 @@ import { Shoe } from "./components/Shoe";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.header}>
-        <Header></Header>
-      </div>
-      <div className={styles.body}>
-        <Shoe id="ss0"></Shoe>
-        <Shoe id="ss01"></Shoe>
-      </div>
-    </main>
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="author" content="newalina" />
+      </Head>
+      <main className={styles.main}>
+        <div className={styles.header}>
+          <Header></Header>
+        </div>
+        <div className={styles.body}>
+          <Shoe id="ss0"></Shoe>
+          <Shoe id="ss01"></Shoe>
+        </div>
+      </main>
+    </>
   );
 }
