6213a1cb644ac150d5483aec772a5374162a33e2
newalina <157907056+newalina@users.noreply.github.com>
2024-07-29 22:45:45 -0400

    add author

--- app/page.tsx
+++ app/page.tsx
@@ -9,20 +9,14 @@ import { Shoe } from "./components/Shoe";
 
 export default function Home() {
   return (
-    <>
-      <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="author" content="newalina" />
-      </Head>
-      <main className={styles.main}>
-        <div className={styles.header}>
-          <Header></Header>
-        </div>
-        <div className={styles.body}>
-          <Shoe id="ss0"></Shoe>
-          <Shoe id="ss01"></Shoe>
-        </div>
-      </main>
-    </>
+    <main className={styles.main}>
+      <div className={styles.header}>
+        <Header />
+      </div>
+      <div className={styles.body}>
+        <Shoe id="ss0"></Shoe>
+        <Shoe id="ss01"></Shoe>
+      </div>
+    </main>
   );
 }
